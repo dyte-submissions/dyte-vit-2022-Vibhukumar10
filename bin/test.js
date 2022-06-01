90663b515260d6efbc2e7c3ca7b56a7f072909f3
Vibhu <vibhukumar1224@gmail.com>
2022-06-02 00:01:50 +0530

    delete test file

--- bin/test.js
+++
@@ -1,8 +0,0 @@
-const utils = require('./utils')
-
-const data = utils.getOldContent(
-  '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',
-  'https://github.com/dyte-in/react-sample-app/'
-)
-
-console.log(data)
