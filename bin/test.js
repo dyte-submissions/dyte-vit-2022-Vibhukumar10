695075586ec9601781c063a00c26488506e90e99
Vibhu <vibhukumar1224@gmail.com>
2022-06-02 00:01:21 +0530

    test file

---
+++ bin/test.js
@@ -0,0 +1,8 @@
+const utils = require('./utils')
+
+const data = utils.getOldContent(
+  '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',
+  'https://github.com/dyte-in/react-sample-app/'
+)
+
+console.log(data)
