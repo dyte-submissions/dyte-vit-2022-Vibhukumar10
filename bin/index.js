505ae6f3ce79a4f391f4ec1d7f6cf5ae93c7bafd
Vibhu <vibhukumar1224@gmail.com>
2022-06-01 14:31:28 +0530

    forking and creating new branch

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,25 @@
 #! /usr/bin/env node
 
-const axios = require('axios')
 const chalk = require('chalk')
 const boxen = require('boxen')
 const yargs = require('yargs')
+const { Octokit } = require('@octokit/rest')
+const { compare, validate } = require('compare-versions')
+const { default: simpleGit, CleanOptions } = require('simple-git')
+simpleGit().clean(CleanOptions.FORCE)
+
+const options = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+}
+
+// when setting all options in a single object
+const git = simpleGit(options)
+
+// console.log(yargs)
+
+const csv = require('csvtojson')
 
 const utils = require('./utils.js')
 
@@ -11,69 +27,105 @@ const usage = chalk.keyword('green')(
   '\nUsage: rely <repo-url> <dependency@version>'
 )
 
-const options = yargs
-  .usage(usage)
-  .option('l', {
-    alias: 'languages',
-    describe: 'List all supported languages.',
+const argv = yargs(process.argv.slice(2))
+  .usage(
+    'Usage: \n\nrely -r <repo-url> <dependency@version>\nrely -i <filname.csv> <dependency@version>'
+  )
+  .option('r', {
+    usage: 'okay',
+    nargs: 2,
+    describe: 'specify single repo-link',
+    type: 'string',
+  })
+  .option('i', {
+    nargs: 2,
+    describe: 'take input from a local .csv file',
+    type: 'string',
+  })
+  .option('u', {
+    alias: 'update',
+    nargs: 0,
+    describe: 'specify the dependency to update',
     type: 'boolean',
-    demandOption: false,
   })
   .help(true).argv
 
-// if (yargs.argv.l == true || yargs.argv.languages == true) {
-//   utils.showAll()
-//   return
-// }
+// console.log(argv)
 
-if (yargs.argv._.length === 0) {
+if (argv.i && argv.r == null) {
+  const csvFileName = argv.i[0]
+  try {
+    const csvPath = utils.getCsvPath(csvFileName)
+    ;(async () => {
+      const data = await csv().fromFile(csvPath)
+      console.log(data)
+    })()
+
+    // var fs = require('fs')
+
+    // var data = fs
+    //   .readFileSync(csvPath)
+    //   .toString() // convert Buffer to string
+    //   .split('\n') // split string to lines
+    //   .map((e) => e.trim()) // remove white spaces for each line
+    //   .map((e) => e.split(',').map((e) => e.trim())) // split each line to array
+
+    // console.log(data.slice(1))
+    // console.log(JSON.stringify(data, '', 2)) // as json
+  } catch (err) {
+    console.log(boxen(chalk.red(err.message)))
+  }
+} else if (argv.r && argv.i == null) {
+  const url = argv.r[0]
+  try {
+    const getUrl = utils.getGithubApiCall(url)
+    const [dependency, version] = argv.r[1].split('@')
+    console.log(dependency, version)
+
+    if (!validate(version)) {
+      throw new Error('Please enter a valid version like: react@17.0.0')
+    }
+
+    utils
+      .getDependencyList(getUrl)
+      .then((data) => {
+        console.log(data)
+        var appVersion = utils.getAppVersion(data, dependency).toString()
+
+        console.log(`\nShowing dependency versions for ${dependency}:`)
+        console.log(
+          chalk.blue(boxen(`current: ${appVersion}\nrequirement: ${version}`))
+        )
+
+        if (!compare(appVersion, version, '>=')) {
+          console.log(
+            chalk.red(
+              `${dependency}@${appVersion} DOESNOT satisfy ${dependency}@${version}\nPlease update using the -u flag`
+            )
+          )
+        } else {
+          console.log(chalk.green('Your dependency is upto date :)'))
+        }
+
+        if (argv.u) {
+          const accessToken = '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'
+          try {
+            // utils.forkRepo(accessToken, url).then(() => console.log('forked'))
+            utils
+              .createBranch(accessToken, url)
+              .then(() => console.log('created'))
+          } catch (err) {
+            console.log(err.message)
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  } catch (err) {
+    console.log(err.message)
+  }
+} else {
+  console.log(boxen(chalk.red(Error('Invalid Input'))))
   utils.showHelp()
-} else if (yargs.argv._[0] != null && yargs.argv._[1] == null) {
-  const url = yargs.argv._[0]
-  const getUrl = utils.getGithubApiCall(url)
-
-  utils
-    .getDependencyList(getUrl)
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-} else if (yargs.argv._[0] != null && yargs.argv._[1] != null) {
-  const url = yargs.argv._[0]
-  const getUrl = utils.getGithubApiCall(url)
-  const [dependency, version] = yargs.argv._[1].split('@')
-
-  console.log(dependency, version)
-
-  utils
-    .getDependencyList(getUrl)
-    .then((data) => {
-      console.log(data)
-      var appVersion = utils.getAppVersion(data, dependency).toString()
-      //   for (var key in Objectdata) {
-      //     if (p.hasOwnProperty(key)) {
-      //       console.log(key + ' -> ' + p[key])
-      //     }
-      //   }
-
-      //   console.log(appVersion)
-      if (appVersion.charAt(0) === '^') {
-        appVersion = appVersion.replace('^', '')
-      }
-
-      console.log(appVersion)
-
-      if (appVersion < version) {
-        console.log('Your version is older than specified!')
-      }
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
 }
-
-console.log(yargs.argv._)
-
-// console.log(usage)
--- bin/utils.js
+++ bin/utils.js
@@ -1,40 +1,80 @@
 const chalk = require('chalk')
+
+const csv = require('csvtojson')
+
 const axios = require('axios')
 const usage = chalk.hex('#83aaff')(
   '\nUsage: rely <repo-url> <dependency@version>'
 )
 
-const getGithubApiCall = (url) => {
-  var getUrl = url.replace('github.com', 'api.github.com/repos')
-  getUrl = getUrl + '/contents/package.json'
+const { Octokit } = require('@octokit/rest')
 
-  return getUrl
+const gh = require('parse-github-url')
+const { compare } = require('compare-versions')
+
+const getGithubApiCall = (url) => {
+  const { owner, name } = gh(url)
+  console.log(owner, name)
+
+  if (!owner || !name) {
+    throw new Error('This is not a valid github link!')
+  } else {
+    var getUrl = `https://api.github.com/repos/${owner}/${name}/contents/package.json`
+    return getUrl
+  }
 }
 
 const getDependencyList = async (url) => {
-  const obj = await axios
-    .get(url)
-    .then((response) => {
-      //   console.log(response.data.content)
-      const obj = JSON.parse(
-        Buffer.from(response.data.content, 'base64').toString('ascii')
-      ).dependencies
-      return obj
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
+  // const obj = await axios
+  //   .get(url)
+  //   .then((response) => {
+  //     //   console.log(response.data.content)
+  //     const obj = JSON.parse(
+  //       Buffer.from(response.data.content, 'base64').toString('ascii')
+  //     ).dependencies
+  //     return obj
+  //   })
+  //   .catch((err) => {
+  //     console.log(
+  //       chalk.red(
+  //         'Either the repository doesnot exist or there is no package.json in its root\n'
+  //       ),
+  //       err.message
+  //     )
+  //   })
 
-  return obj
+  const { owner, name } = gh(url)
+  const octokit = new Octokit({
+    auth: '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',
+  })
+
+  const obj = await octokit.request(
+    'GET /repos/{owner}/{repo}/contents/{path}',
+    {
+      owner: owner,
+      repo: name,
+      path: 'package.json',
+    }
+  )
+  return JSON.parse(Buffer.from(obj.data.content, 'base64').toString('ascii'))
+    .dependencies
 }
 
+const validateDependency = async () => {}
+
 const getAppVersion = (obj, dependency) => {
+  var appVersion
   for (var key in obj) {
     if (obj.hasOwnProperty(key) && key === dependency) {
       //   console.log(obj[key])
-      return obj[key]
+      appVersion = obj[key]
+      break
     }
   }
+  if (appVersion.charAt(0) < '0' || appVersion.charAt(0) > '9') {
+    appVersion = appVersion.slice(1)
+  }
+  return appVersion
 }
 
 const showHelp = () => {
@@ -44,13 +84,85 @@ const showHelp = () => {
     '\t--version\t      ' + 'Show version number.' + '\t\t' + '[boolean]\r'
   )
   console.log(
-    '    -l, --languages\t' +
+    '    -i, --inp\t' +
       '      ' +
-      'List all languages.' +
+      'Take input from a .csv file' +
       '\t\t' +
       '[boolean]\r'
   )
   console.log('\t--help\t\t      ' + 'Show help.' + '\t\t\t' + '[boolean]\n')
+}
+
+const parseCsv = async (csvPath) => {
+  return await csv().fromFile(csvPath)
+}
+
+const getCsvPath = (csvFileName) => {
+  if (csvFileName.slice(-4) !== '.csv') {
+    throw new Error('Cheeky! Only [.csv] file-type is supported!')
+  } else {
+    const csvPath = process.cwd() + '\\' + csvFileName
+    // console.log(csvPath)
+    return csvPath
+  }
+}
+
+const forkRepo = async (accessToken, url) => {
+  const { owner, name } = gh(url)
+
+  try {
+    const octokit = new Octokit({
+      auth: accessToken,
+    })
+
+    await octokit.request('POST /repos/{owner}/{repo}/forks', {
+      owner: owner,
+      repo: name,
+    })
+  } catch (err) {
+    console.log(err.message)
+  }
+
+  // console.log('done')
+
+  // await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+  //   owner: 'Vibhukumar10',
+  //   repo: name,
+  //   ref: 'refs/heads/featureA',
+  //   sha: 'aa218f56b14c9653891f9e74264a383fa43fefbd',
+  // })
+
+  // const octokit = new Octokit({
+  //   auth: accessToken,
+  // })
+
+  // await octokit.request('POST /repos/{owner}/{repo}/pulls', {
+  //   owner: owner,
+  //   repo: name,
+  //   title: 'Amazing new feature',
+  //   body: 'Please pull these awesome changes in!',
+  //   head: 'octocat:new-feature',
+  //   base: 'master',
+  // })
+}
+
+const createBranch = async (accessToken, url) => {
+  const { owner, name } = gh(url)
+
+  try {
+    const octokit = new Octokit({
+      auth: accessToken,
+    })
+
+    await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+      owner: owner,
+      repo: name,
+      ref: 'refs/heads/featureA',
+      sha: 'aa218f56b14c9653891f9e74264a383fa43fefbe',
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 module.exports = {
@@ -58,4 +170,8 @@ module.exports = {
   showHelp,
   getDependencyList,
   getAppVersion,
+  parseCsv,
+  getCsvPath,
+  forkRepo,
+  createBranch,
 }
